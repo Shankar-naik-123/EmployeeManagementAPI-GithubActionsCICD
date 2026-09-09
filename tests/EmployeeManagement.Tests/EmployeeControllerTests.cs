b240387ccf11936b47cb654280e8ab15e0ead9f1
Shankar <myselfshankarnaik@gmail.com>
2026-09-09 17:18:23 +0530

    Added UnitTests

--- tests/EmployeeManagement.Tests/EmployeeControllerTests.cs
+++ tests/EmployeeManagement.Tests/EmployeeControllerTests.cs
@@ -1,5 +1,7 @@
 ﻿using EmployeeManagement.API;
 using EmployeeManagement.API.Controllers;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
 
 namespace EmployeeManagement.Tests
 {
@@ -26,6 +28,16 @@ namespace EmployeeManagement.Tests
             var result = controller.AddEmployee(e);
             Assert.IsType<Microsoft.AspNetCore.Mvc.OkObjectResult>(result);
             Assert.Contains(controller.GetEmployees(), e => e.Name == "Test Name");
+        }
+        
+        [Fact]
+        public void HealthCheck_ReturnsOkWithHealthyMessage()
+        {
+            var controller = new EmployeeController();
+            var result = controller.HealthCheck();
+            
+            Assert.Equal("Sever is healthy!",  ((OkObjectResult)result).Value);
+           ;
         }
     }
 }
